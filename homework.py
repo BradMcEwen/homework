c8017d2d302a15226ea843d71708d411c6b59a8c
BradMcEwen <mcewen.brad30@gmail.com>
2024-04-30 01:57:59 -0400

    4/29 homework

---
+++ homework.py
@@ -0,0 +1,40 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+
+print("How are you doing today?")
+      
+emotion = "happy", "sad", "nervous"
+
+if emotion == "happy":
+        print("That's great to hear!")
+        if emotion == "sad":
+            print("I hope your day gets better!")
+        else:
+              print("Keep your head up!")
+
+
+
+
+pi_value = 3.14
+user_age = 25
+USER_LOCATION = "NEW YORK"
+max_limit = 1000
+
+
+bread = 4.73
+peanut_butter = 7.12
+jelly = 4.15
+groceries = bread + peanut_butter + jelly
+print(groceries)
+
+savings = 10000
+interest = .07
+total_interest = savings * interest
+new_savings = total_interest + savings
+print(new_savings)
